901f1fa902c651568033eb25112fcdf9f7679ef3
Eric Ogren <eogren@gmail.com>
2014-11-09 00:49:30 -0500

    status updater stub

---
+++ service-layer/status-updater/src/main/java/com/eogren/link_checker/status_updater/StatusUpdaterApplication.java
@@ -0,0 +1,14 @@
+package com.eogren.link_checker.status_updater;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class StatusUpdaterApplication {
+    public static void main(String[] args) {
+        Logger logger = LoggerFactory.getLogger(StatusUpdaterApplication.class);
+
+        logger.info("I'm alive!!");
+
+        System.out.println("Testing 1 2 3");
+    }
+}
