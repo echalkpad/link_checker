6ac2c6e77f2db4950392130d85775df275c845f6
Eric Ogren <eogren@gmail.com>
2014-12-22 21:42:52 -0500

    SU-56 Express app to server client code

---
+++ admin-interface/server.js
@@ -0,0 +1,18 @@
+var config = require('./config');
+var compression = require('compression')
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(compression());
+app.use(morgan('short'));
+app.use(express.static('client'));
+
+app.use(function(req, res, next){
+  res.status(404).send('Sorry cant find that!');
+});
+
+var server = app.listen(config.port, function() {
+    console.log('Express is alive! Listening on port ' + config.port);
+});
