4fe33511bf1f3665c2ad4d7727a1c97da194720d
Eric Ogren <eogren@gmail.com>
2014-12-27 00:19:05 -0500

    SU-60 SU-61 SU-62 use queue for syncer

--- admin-interface/client/js/api_client/syncer.js
+++ admin-interface/client/js/api_client/syncer.js
@@ -1,6 +1,7 @@
 var Q = require('q');
 var request = require('superagent');
 require('q-superagent')(request);
+var _ = require('underscore');
 
 var min_retry = 1000;
 var retry = min_retry;
@@ -9,6 +10,10 @@ var max_retry = 30000;
 var newRetry = function() {
     return Math.max(max_retry, retry * retry);
 };
+
+var UPDATE_PAGES = "UPDATE_PAGES";
+var ADD_MONITORED_PAGE = "ADD_MONITORED_PAGE";
+var DELETE_MONITORED_PAGE = "DELETE_MONITORED_PAGE";
 
 /**
 * sendWithRetry sends ajax requests with a retry on failure.
@@ -39,6 +44,7 @@ var sendWithRetry = function(senderFn) {
 
 var Syncer = function(flux) {
     this.flux = flux;
+    this.queue = [];
 
     // Subscribe to MPS events
     var store = flux.store("MonitoredPageStore");
@@ -54,19 +60,29 @@ var Syncer = function(flux) {
 };
 
 Syncer.prototype.updateMonitoredPages = function() {
+    this._addOp({op: UPDATE_PAGES});
+};
+
+Syncer.prototype._processUpdateMonitoredPages = function() {
     var get = sendWithRetry(function() {
         return request.get('/api/v1/monitored_page/').q();
     });
 
     var self = this;
-    get.then(function resolveData(data) {
+    var p = get.then(function resolveData(data) {
         self.flux.actions.updateMonitoredPages(data.body);
     }, function onError(why) {
         console.log("Error: " + why);
     });
+
+    return p;
 };
 
 Syncer.prototype.addMonitoredPage = function(url) {
+    this._addOp({op: ADD_MONITORED_PAGE, url: url});
+};
+
+Syncer.prototype._processAddMonitoredPage = function(url) {
     var self = this;
     var post = sendWithRetry(function() {
         return request
@@ -76,16 +92,22 @@ Syncer.prototype.addMonitoredPage = function(url) {
 
     });
 
-    post.then(function resolveData(data) {
+    var p = post.then(function resolveData(data) {
         self.flux.actions.addSynced(url);
     },
 
     function error(why) {
         // TODO: fill me in
     });
+
+    return p;
 };
 
 Syncer.prototype.deleteMonitoredPage = function(url) {
+    this._addOp({op: DELETE_MONITORED_PAGE, url: url});
+};
+
+Syncer.prototype._processDeleteMonitoredPage = function(url) {
     var self = this;
     var deleteOp = sendWithRetry(function() {
         return request
@@ -93,12 +115,49 @@ Syncer.prototype.deleteMonitoredPage = function(url) {
                 .q();
     });
 
-    deleteOp.then(function resolveData(data) {
+    var p = deleteOp.then(function resolveData(data) {
         self.flux.actions.deleteSynced(url);
     },
 
     function error(why) {
         // TODO: fill me in
+    });
+
+    return p;
+};
+
+Syncer.prototype._addOp = function(op) {
+    this.queue.push(op);
+    if (_.size(this.queue) == 1) {
+        this._processQueue();
+    }
+};
+
+Syncer.prototype._processQueue = function() {
+    if (_.size(this.queue) === 0) {
+        return;
+    }
+
+    var op = this.queue.shift();
+    var self = this;
+    var p;
+
+    switch(op.op) {
+    case UPDATE_PAGES:
+        p = this._processUpdateMonitoredPages();
+        break;
+    case ADD_MONITORED_PAGE:
+        p = this._processAddMonitoredPage(op.url);
+        break;
+    case DELETE_MONITORED_PAGE:
+        p = this._processDeleteMonitoredPage(op.url);
+        break;
+    default:
+        throw new Error("Unknown op " + op + "!");
+    }
+
+    p.fin(function() {
+        self._processQueue();
     });
 };
 
